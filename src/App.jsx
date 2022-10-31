df93c5e819d2fb7826765953080f13de8f1d8e4f
Brayan Albadam <blackthorn_1994@hotmail.com>
2022-10-31 18:30:29 -0500

    Gastos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import Header from './components/Header'
 import Modal from './components/Modal'
+import ListadoGastos from './components/ListadoGastos'
 import IconoNuevoGasto from'./img/nuevo-gasto.svg'
 import{generarId} from './helpers'
 
@@ -21,7 +22,8 @@ const handleNuevoGasto = ( ) => {
   },500);
 }
 const guardarGasto = gasto => {
-  gasto.id = generarId()
+  gasto.id = generarId( )
+  gasto.fecha=Date.now( );
   setGastos([...gastos, gasto])
   setAnimarModal(false)
   setTimeout(() => {
@@ -38,11 +40,19 @@ const guardarGasto = gasto => {
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
       {isValidPresupuesto && (
+        <>
+        <main>
+
+          <ListadoGastos
+          gastos={gastos}
+          />
+        </main>
       <div className="nuevo-gasto">
       <img src={IconoNuevoGasto} alt="Nuevo gasto"
       onClick={handleNuevoGasto}
       />
       </div>
+      </>
       )}
 
       {modal && <Modal
